c07616cda1b8e6fc8cf7553018e3aea7fa3861d9
ersk <psycopsymon@hotmail.com>
2016-10-01 10:44:11 +0100

    fgfrtdg

---
+++ Source/Model/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Animal : LivingStats
+    {
+        // 4 llama milk? 4 milk?
+        public List<ItemCount> MilkOutput { get; set; }
+
+        // 6 eagle eggs? 6 egsg?
+        public List<ItemCount> EggOutput { get; set; }
+
+        // wool
+        public List<ItemCount> ShearingOutput { get; set; }
+
+        //venom
+        public List<ItemCount> PoisonOutput { get; set; }
+
+        /*
+         * pelt
+         * bones
+         * ivory
+         * scales
+         * feathers
+         * fur
+         * oil
+         * poison
+         */
+        public List<ItemCount> SlaughterOutput { get; set; }
+
+        public uint Volatility { get; set; }
+    }
+
+    public enum FoodTypeEnum
+    {
+        Plant,
+        Animal
+    }
+
+    /* Name
+     * Type (parent)
+     * Size
+     * Weight
+     */ 
+    class FoodItem : Item
+    {
+        // egg = 3
+        // carrot = 4
+        // pig leg = 15
+        public int Value { get; set; }
+        public FoodTypeEnum Type { get; set; }
+    }
+
+    // food/animal/dairy/milk/cow_milk
+    // food/animal/egg/eagle_egg
+
+}
---
+++ Source/Model/LivingStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class LivingStats
+    {
+        public uint Charge { get; set; }
+        public uint Attack { get; set; }
+        public uint Defence { get; set; }
+        public uint Toughness { get; set; }
+        public uint Speed { get; set; }
+        public uint Stamina { get; set; }
+        public uint Courage { get; set; }
+        public uint Fear { get; set; }
+    }
+}
